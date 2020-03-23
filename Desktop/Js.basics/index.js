4d0e08f544b36247df7b2d1a419e4d3987ae8c75
Armmand Alston <armmand33@gmail.com>
2020-03-23 16:20:17 -0400

    Updated index.js

--- Desktop/Js.basics/index.js
+++ Desktop/Js.basics/index.js
@@ -1,2 +1,35 @@
  //Comment This was a test javascript code!
  console.log("Hello World"); 
+
+ alert('This is really working');
+
+ //Variables 
+ //Cannot be a reserved keyword. (Let and Var are Reserved).
+ //Should be menaingful.
+ //Cannot start with a number (1name).
+ //Cannot contain a space it hypen (-).
+ //Are case-sensitive.
+ var name2 =('Alston');
+ console.log(name2);
+
+ var wing =('John Doe');
+ console.log(wing);
+
+ let name =('Armmand');
+ console.log(name);
+
+ //Constants.
+ //Constants are like Var and Let. 
+ //You only use constants when you dont want your varible to change.
+ const interestRate = 0.3;
+ interestRate = 1;
+ console.log(interestRate);
+
+ var interestRate = 0.3;
+ interestRate = 1;
+ console.log(interestRate);
+
+ let interestRate =0.3;
+ interestRate = 1;
+ console.log(interestRate);
+
