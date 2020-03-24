541839dbda704e92fde39f78a5aae781e62a1aa6
Armmand Alston <armmand33@gmail.com>
2020-03-23 20:58:11 -0400

    Update index2.js

--- Desktop/Js.basics/index.js
+++ Desktop/Js.basics/index.js
@@ -1,54 +1,14 @@
-let person = {
-    name: 'John',
-    lastName: 'Alston',
-    age: 30
-};
-//Dot Nottaion
-person.name = 'Armmand';
-console.log(person.name);
+//Arrays
 
-//Bracket Notation.
+//Example1 using (var)
+var selectedColors = ['red','blue'];
 
-//First Example.
-person ['name'] = 'Mary';
-console.log(person.name);
+//Example2 using (let).
+let selectedColors = ['red','blue'];
 
-//Second Example.
-let selection = 'name';
-person [selection] = 'Tom';
-console.log(person.name);
-////////////////////////////////////////////////////////////
- //Comment This was a test javascript code!
- console.log("Hello World"); 
+//Added to the Array.
+selectedColors[2] = 'green';
 
- alert('This is really working');
-
- //Variables 
- //Cannot be a reserved keyword. (Let and Var are Reserved).
- //Should be menaingful.
- //Cannot start with a number (1name).
- //Cannot contain a space it hypen (-).
- //Are case-sensitive.
- var name2 =('Alston');
- console.log(name2);
-
- var wing =('John Doe');
- console.log(wing);
-
- let name =('Armmand');
- console.log(name);
-////////////////////////////////////////////////////////////
- //Constants.
- //Constants are like Var and Let. 
- //You only use constants when you dont want your varible to change.
- const interestRate = 0.3;
- interestRate = 1;
- console.log(interestRate);
-
- var interestRate = 0.3;
- interestRate = 1;
- console.log(interestRate);
-
- let interestRate =0.3;
- interestRate = 1;
- console.log(interestRate);
+//This block of code prints the anwser to the console.
+console.log(selectedColors);
+tou
---
+++ Desktop/Js.basics/index2.js
@@ -0,0 +1,16 @@
+//Functions
+
+//Array Examples
+function greet(name,lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+greet('John', 'Smith');
+
+
+//Array Example Performing a task.
+
+//Calculating a value
+function square(number) {
+    return number * number;
+}
+console.log(square(2));
